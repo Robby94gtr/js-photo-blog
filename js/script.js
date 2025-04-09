9195f31b96510b6add2f221c4a6607032385ee6c
Robby94gtr <robertogramazio94@gmail.com>
2025-04-09 15:55:48 +0200

    [UPD] Ended Overlay

--- js/script.js
+++ js/script.js
@@ -11,6 +11,16 @@ const endpoint = `https://lanciweb.github.io/demo/api/pictures/`
 
 let immagini = [];
 
+// overlay
+const overlay = document.querySelector('.overlay')
+// overlay button
+const button = document.querySelector('button')
+// overlay image container
+const overlayImage = document.getElementById('overlay-img-container')
+
+
+
+
 // Funzione per creare la colonna Polaroid
 
 const createPolaroid = (array) => {
@@ -20,7 +30,9 @@ const createPolaroid = (array) => {
         let {id, title, date, url} = photo;
         // aggiungo id al img-container
         element +=  `<div class="column bg-light  img-container d-flex flex-wrap flex-column position-relative pt-3 pb-4 px-3 my-4 ${id}">
-                    <img class="w-100" src="${url}" alt="">
+                    <div class="polaroid-img w-100 ${id}">
+                            <img class="w-100" src="${url}" alt="immagine">
+                        </div>
                     <div class="title">${title}</div>
                     <div class="date">${date}</div>
                         <div class="pin">
@@ -36,6 +48,20 @@ const createPolaroid = (array) => {
     return element
 }
 
+// function to create the correct image to display in overlay
+const correctImage = (url) => {
+    let element = `<img src="${url}" alt="">`
+    overlayImage.innerHTML = element;
+}
+
+// clicking overlay button will add overlay-hidden on overlay
+button.addEventListener('click', (e) => {
+    e.preventDefault;
+    overlay.classList.remove('overlay-active')
+    overlay.classList.add('overlay-hidden')
+})
+
+
 // Chiamata axios per prelevare tutte le informazioni che mi servono
 axios.get(endpoint)
     .then((resp) => {
@@ -49,6 +75,46 @@ axios.get(endpoint)
         const elements = createPolaroid(immagini);
 
         row.innerHTML += elements;
+
+        // once images are presen in DOM, I can get polaroid-img to open overlay
+        const polaroidImg = document.querySelectorAll('.polaroid-img')
+        console.log(polaroidImg)
+        polaroidImg.forEach((image) => {
+            image.addEventListener('click', (e) => {
+                overlay.classList.remove('overlay-hidden')
+                overlay.classList.add('overlay-active')
+
+                // if(image.classList.contains(1)){
+                //     correctImage(images[0].url);
+                // }
+                // else if(image.classList.contains(2)){
+                //     correctImage(images[1].url);
+                // }
+                // else if(image.classList.contains(3)){
+                //     correctImage(images[2].url);
+                // }
+                // else if(image.classList.contains(4)){
+                //     correctImage(images[3].url);
+                // }
+                // else if(image.classList.contains(5)){
+                //     correctImage(images[4].url);
+                // }
+                // else{
+                //     correctImage(images[5].url);
+                // }
+
+                // better way
+
+                const classIndex = parseInt([...image.classList].find(cls => !isNaN(cls))) - 1;
+                // image is a Nodelist, not a classic array. I'll use spread operator to transform it into a classic array
+                // ...image.classList access the classList of the clicked image
+                // .find(cls => !isNaN(cls)) find the first class that is a number
+                // parseInt transform it into a number
+                correctImage(immagini[classIndex].url);
+            })
+        });
         
     });
+
+
 
